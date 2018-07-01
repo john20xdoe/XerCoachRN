2c6a984f1db375d4dbb5abbc9dd78af3930a21f5
Lee Alexis <labermejo@eml.cc>
2018-07-01 22:10:52 +0800

    Ignore for isMounted depreciation warning

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //
 import App from './src/App';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 AppRegistry.registerComponent('xercoachRN', () => App);
