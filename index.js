a3b48e574bff0f52b5f0c71f2bd6df2a054e97f4
Lee Alexis Bermejo <lbermejo@isbx.com>
2018-07-04 08:52:34 +0800

    Yellow box ignore class RCTCxxModule

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import App from './src/App';
 import { AppRegistry, YellowBox } from 'react-native';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Class RCTCxxModule']);
 AppRegistry.registerComponent('xercoachRN', () => App);
